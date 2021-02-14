5a56986ac702420948031bffd2b582697a19fb82
Spiegel <spiegel.im.spiegel@gmail.com>
2021-02-14 21:27:39 +0900

    New post

--- archetypes/golang.md
+++ archetypes/golang.md
@@ -1,6 +1,6 @@
 ---
 title: "" # 記事のタイトル
-emoji: "⌨" # アイキャッチとして使われる絵文字（1文字だけ）
+emoji: "💻" # アイキャッチとして使われる絵文字（1文字だけ）
 type: "tech" # "tech" : 技術記事 / "idea" : アイデア記事
 topics: ["go", "programming"] # タグ。["markdown", "rust", "aws"] のように指定する
 published: false # 公開設定（true で公開）
---
+++ code/cov19jpn/sample1.go
@@ -0,0 +1,43 @@
+// +build run
+
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/spiegel-im-spiegel/cov19jpn/entity"
+	"github.com/spiegel-im-spiegel/cov19jpn/fetch"
+	"github.com/spiegel-im-spiegel/cov19jpn/filter"
+	"github.com/spiegel-im-spiegel/cov19jpn/values/prefcodejpn"
+)
+
+func main() {
+	r, err := fetch.Web(context.Background(), &http.Client{})
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	defer r.Close()
+	es, err := fetch.Import(
+		r,
+		filter.New(
+			prefcodejpn.TOTTORI,
+			prefcodejpn.SHIMANE,
+			prefcodejpn.OKAYAMA,
+			prefcodejpn.HIROSHIMA,
+			prefcodejpn.YAMAGUCHI,
+		),
+	)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	list := entity.NewList(es)
+	list.Sort()
+	_, _ = io.Copy(os.Stdout, bytes.NewReader(list.EncodeCSV()))
+}
